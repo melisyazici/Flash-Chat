91f2bfa5d0c949b94ca4234f09c580f4b30b7e87
Melis Yazıcı <melis.yazici95@icloud.com>
2022-05-12 13:02:03 +0200

    Initial Commit

---
+++ Flash Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flash Chat
+//
+//  Created by Melis Yazıcı on 12.05.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
